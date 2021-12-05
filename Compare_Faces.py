7283dfcb4e8b0c4e178f9879657c4287ba436320
LunineE <31840446+LunineE@users.noreply.github.com>
2021-12-05 20:30:16 +0900

    Update Compare_Faces.py

--- Compare_Faces.py
+++ Compare_Faces.py
@@ -1,9 +1,10 @@
 import boto3
+import os
 
 def compare_faces(bucket, source, target, region='us-east-1'):
     rekognition = boto3.client('rekognition', region)
     response = rekognition.compare_faces(
-        SimilarityThreshold=0, #예제 기본 수치 80
+        SimilarityThreshold=0,
         SourceImage={
             'S3Object': {
                 'Bucket': bucket,
@@ -20,7 +21,7 @@ def compare_faces(bucket, source, target, region='us-east-1'):
 
     for faceMatch in response['FaceMatches']:
         similarity = round(float(faceMatch['Similarity']), 2)
-        if (similarity >= 95):
+        if (similarity >= 90):
             print(str(similarity) + '%')
             print('동일 인물 입니다')
         elif (similarity >= 70):
@@ -34,11 +35,12 @@ def compare_faces(bucket, source, target, region='us-east-1'):
 
 
 def main():
-    bucket = 'comparefacesbucket'
-    source_file = 'source.png'
-    target_file = 'target.png'
+    bucket = input('버킷이름 입력 : ') # 'comparefacesbucket'
+    source_file = input('소스파일 입력 : ') # 'source01.png'
+    target_file = input('타겟파일 입력 : ') # 'target01.png'
     face_matches = compare_faces(bucket, source_file, target_file)
     print("Face matches: " + str(face_matches))
+    os.system('pause')
 
 
 if __name__ == "__main__":
